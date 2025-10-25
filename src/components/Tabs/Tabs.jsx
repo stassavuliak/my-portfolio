e1b3497057c150e61d3cc8f43e46a2938a7a2ae2
Stanislav Savuliak <stassavulak@gmail.com>
2025-10-25 23:28:18 +0200

    refactor: move data to JSON and use map for TestimonialsSlider and Tabs

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,69 +1,22 @@
 import { useState } from "react";
 import './Tabs.scss';
 
+import tabsData from '../../data/tabs.json';
+import itemsData from '../../data/tabsItems.json';
+
 const TabsWithFilter = () => {
-  // Define the tabs (categories)
-  const tabs = [
-    { id: "all", label: "All" },
-    { id: "uiUx", label: "UI/UX" },
-    { id: "webDesign", label: "Web Design" },
-    { id: "appDesign", label: "App Design" },
-    { id: "graphicDesign", label: "Graphic Design" },
-  ];
-
-  // Define the items with their categories
-  const items = [
-    {
-      id: 1,
-      image: "/images/img/proj1.png",
-      title: "AirCalling Landing Page Design",
-      categoryName: "UI/UX & Web Design",
-      categories: ["uiUx", "webDesign"],
-    },
-    {
-      id: 2,
-      image: "/images/img/proj2.png",
-      title: "Business Landing Page Design ",
-      categoryName: "App Design",
-      categories: ["appDesign"],
-    },
-    {
-      id: 3,
-      image: "/images/img/proj3.png",
-      title: "Ecom Web Page Design ",
-      categoryName: "Graphic Design & App Design",
-      categories: ["graphicDesign", "appDesign"],
-    },
-    {
-      id: 4,
-      image: "/images/img/proj1.png",
-      title: "Business Landing Page Design",
-      categoryName: "App Design",
-      categories: ["appDesign"],
-    },
-    {
-      id: 5,
-      image: "/images/img/proj2.png",
-      title: "AirCalling Landing Page Design",
-      categoryName: "UI/UX",
-      categories: ["uiUx"],
-    },
-  ];
-
-  // State to track the active tab
   const [activeTab, setActiveTab] = useState("all");
 
-  // Filter items based on the active tab
   const filteredItems =
     activeTab === "all"
-      ? items
-      : items.filter((item) => item.categories.includes(activeTab));
+      ? itemsData
+      : itemsData.filter(item => item.categories.includes(activeTab));
 
   return (
     <div className="tabs">
       {/* Tab Headers */}
       <div className="tabs__header">
-        {tabs.map((tab) => (
+        {tabsData.map(tab => (
           <button
             key={tab.id}
             className={`tabs__button ${activeTab === tab.id ? "active" : ""}`}
@@ -78,14 +31,13 @@ const TabsWithFilter = () => {
       <div className="tabs__content">
         {filteredItems.length > 0 ? (
           <div className="tabs__list">
-            {filteredItems.map((item) => (
+            {filteredItems.map(item => (
               <a href="#!" key={item.id} className="tabs__item">
                 <img 
                   src={item.image} 
                   alt={item.title} 
                   className="tabs__item-image"
                 />
-
                 <span>{item.categoryName}</span>
                 <h4>{item.title}</h4>
               </a>
--- src/components/TestimonialsSlider/TestimonialsSlider.jsx
+++ src/components/TestimonialsSlider/TestimonialsSlider.jsx
@@ -1,13 +1,11 @@
-// import Swiper core and required modules
 import { Pagination, Autoplay} from 'swiper/modules';
-
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './TestimonialsSlider.scss';
+
+import testimonialsData from '../../data/testimonials.json'
 
 const TestimonialsSlider = () => {
   return (
@@ -15,8 +13,6 @@ const TestimonialsSlider = () => {
       pagination={true}
       spaceBetween={50}
       slidesPerView={1}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
       modules={[Pagination, Autoplay]}
       loop={true}
       autoplay={{
@@ -25,57 +21,18 @@ const TestimonialsSlider = () => {
       }}
     >
 
-
-      <SwiperSlide>
-        <div className="testimonials__slide">
-          <img src="images/img/test1.png" className='testimonials__img' alt="" />
-          <div className="testimonials__info">
-            <p>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.</p>
-            <b>Name</b>
-            <span>CEO</span>
+      {testimonialsData.map((testimonial, index) => (
+        <SwiperSlide key={index}>
+          <div className="testimonials__slide">
+            <img src={testimonial.img} className="testimonials__img" alt={testimonial.name} />
+            <div className="testimonials__info">
+              <p>{testimonial.text}</p>
+              <b>{testimonial.name}</b>
+              <span>{testimonial.role}</span>
+            </div>
           </div>
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide>
-        <div className="testimonials__slide">
-          <img src="images/img/test2.png" className='testimonials__img' alt="" />
-          <div className="testimonials__info">
-            <p>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.</p>
-            <b>Name</b>
-            <span>CEO</span>
-          </div>
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide>
-        <div className="testimonials__slide">
-          <img src="images/img/test1.png" className='testimonials__img' alt="" />
-          <div className="testimonials__info">
-            <p>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.</p>
-            <b>Name</b>
-            <span>CEO</span>
-          </div>
-        </div>
-      </SwiperSlide>
-
-      <SwiperSlide>
-        <div className="testimonials__slide">
-          <img src="images/img/test2.png" className='testimonials__img' alt="" />
-          <div className="testimonials__info">
-            <p>Lorem ipsum dolor sit amet consectetur. In enim cursus odio accumsan. Id leo urna velit neque mattis id tellus arcu condimentum. Augue dictum dolor elementum convallis dignissim malesuada commodo ultrices.</p>
-            <b>Name</b>
-            <span>CEO</span>
-          </div>
-        </div>
-      </SwiperSlide>
-
-      
-
-   
-
-
-
+        </SwiperSlide>
+      ))}
     </Swiper>
   );
 };
